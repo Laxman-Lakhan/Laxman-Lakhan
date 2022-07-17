5ec16adb60b01f3c75e9c6d1252b0e3a523897a6
Laxman Singh <68061206+Laxman-Lakhan@users.noreply.github.com>
2022-07-17 23:36:48 +0530

    Update rating_chart.py

--- Codes/rating_chart.py
+++ Codes/rating_chart.py
@@ -86,8 +86,8 @@ def main():
 if __name__ == "__main__":
     plot, rl, date = main()
     print (plot, '\n')
-    print('Maximum Rating: {}'.format(max(rl)), end = '                      ')
-    print('Average Rating: {}'.format(round(np.mean(rl))), end = '                      ')
+    print('Maximum Rating: {}'.format(max(rl)))
+    print('Average Rating: {}'.format(round(np.mean(rl))))
     print('Current Rating: {}'.format(rl[-1]), '\n')
     print('Last Game Played On: ',date)
     
