c1fb6386086ed51e2b9080296e4bc3e7d27fc0af
Laxman Singh <68061206+Laxman-Lakhan@users.noreply.github.com>
2023-11-19 19:04:47 +0530

    Update rating_chart.py

--- Codes/rating_chart.py
+++ Codes/rating_chart.py
@@ -90,16 +90,16 @@ if __name__ == "__main__":
 
     print (plot, '\n')
 
-    print('Wins', 'Draws', 'Losses', 'Performance Rating', sep = ' '*8, end = '\n')
-    print((' {} ').format(W), 
-          (' {}  ').format(D), 
-          ('   {}  ').format(L), 
-          ('       {}       ').format(int(pl[-1])), sep = ' '*8, end = '\n\n')
+    print('Wins', 'Draws', 'Losses', 'Performance Rating', sep = ' '*8)
+    print((' '+'{}'+' ').format(W), 
+          (' '+'{}'+' '*2).format(D), 
+          (' '*3+'{}'+' '*2).format(L), 
+          (' '*7+'{}'+' '*7).format(int(pl[-1])), sep = ' '*8, end = '\n\n')
 
     print('Current Rating', 'Highest Rating', 'Average Rating', sep = ' '*8, end = '\n')
-    print(('     {}     ').format(rl[-1]), 
-          ('     {}     ').format(max(rl)), 
-          ('     {}     ').format(round(np.mean(rl))), 
+    print((' '*5+'{}'+' '*5).format(rl[-1]), 
+          (' '*5+'{}'+' '*5).format(max(rl)), 
+          (' '*5+'{}'+' '*5).format(round(np.mean(rl))), 
           sep = ' '*8, end = '\n\n')
 
     print('Last Game Played On:',date)
